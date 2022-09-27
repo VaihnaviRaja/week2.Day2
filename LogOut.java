b389ae062074c6e2e0af693129f6e6ee0bac2790
VaihnaviRaja <113991779+VaihnaviRaja@users.noreply.github.com>
2022-09-27 23:01:24 +0530

    Add files via upload

---
+++ LogOut.java
@@ -0,0 +1,23 @@
+package Week2.Day2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LogOut {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://acme-test.uipath.com/login");
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("kumar.testleaf@gmail.com");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("leaf@12");
+		driver.findElement(By.xpath("//button[@class='btn btn-primary']")).click();
+		System.out.println(driver.getTitle());
+		driver.findElement(By.xpath("//a[contains(text(),'Log Out')]")).click();
+		driver.close();
+		
+	}
+
+}
